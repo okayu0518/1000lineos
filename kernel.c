3197f9ee6d67e5db714c24b768a189544ffbc636
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-05-10 14:04:32 +0900

    restart!!!! \n printf worked finally!

--- kernel.c
+++ kernel.c
@@ -1,7 +1,30 @@
 #include "kernel.h"
 #include "common.h"
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
 
+extern char __bss[], __bss_end[], __stack_top[];
 
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+void boot(void) {
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n"
+        "j kernel_main\n"
+        :
+        : [stack_top] "r" (__stack_top)
+    );
+}
+
+//------------------------------------------------
 extern char __bss[], __bss_end[], __stack_top[];
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
