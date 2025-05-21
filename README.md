374cfe88c4d770dd9e6cb1154ce5c002cbe4dd42
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-05-21 21:12:01 +0900

    update

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 # CSR (control and status registers)
 - CSRは、プロセッサの動作モードの制御、ステータス情報の保持、例外処理、割り込み制御、性能カウンタなど、多岐にわたる特権的な機能や情報にアクセスするためのレジスタ群です。
 - アクセス: csrrw (CSR Read and Write), csrrs (CSR Read and Set), csrrc (CSR Read and Clear) などの専用命令を使ってアクセスします。
+
+```
+[okayu0518@x1:~/projects/1000lineos] (main)
+% llvm-nm shell.bin.o | grep _binary_shell_bin_size
+00010260 A _binary_shell_bin_size
+
+[okayu0518@x1:~/projects/1000lineos] (main)
+% ls -al shell.bin
+-rwxrwxr-x 1 okayu0518 okayu0518 66144 May 21 20:58 shell.bin
+
+[okayu0518@x1:~/projects/1000lineos] (main)
+% python3 -c 'print(0x10260)'
+66144
+```
--- run.sh
+++ run.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -xue
 
-QEMU=qemu-system-riscv32
+OBJCOPY=/bin/llvm-objcopy
 
 # clangのパス (Ubuntuの場合は CC=clang)
 CC=clang
-
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
-# カーネルをビルド
+# build shell
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+# 生バイナリ形式の実行イメージを、C言語に埋め込める形式に変換する処理です。
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
+# build kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-  kernel.c common.c
+  kernel.c common.c shell.bin.o
+
+QEMU=qemu-system-riscv32
 
 # QEMUを起動
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
