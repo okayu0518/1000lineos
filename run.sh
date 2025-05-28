bd3675833f1235e50c99b9d7cd96f1327d948df8
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-05-28 15:50:20 +0900

    finish 15 disk rw

--- run.sh
+++ run.sh
@@ -21,4 +21,7 @@ QEMU=qemu-system-riscv32
 
 # QEMUを起動
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-  -kernel kernel.elf -monitor telnet:127.0.0.1:4444,server,nowait
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=lorem.txt,format=raw,if=none \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
+    -kernel kernel.elf
